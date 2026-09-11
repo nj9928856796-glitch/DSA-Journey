4e681358d840d029cf0ca13f33ecd308e0763b67
Neha jangid <nj9928856796@gmail.com>
2026-09-11 18:10:44 +0530

    Solve LC643-Maximum-Average-Subarray-I.java

---
+++ Sliding-Window/LC643-Maximum-Average-Subarray-I.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int n = nums.length;
+        int current = 0;
+        for (int i = 0; i < k; i++) {
+            current += nums[i];
+        }
+        int max = current;
+        for (int i = 1; i <= n - k; i++) {
+            current = current - nums[i - 1] + nums[i + k - 1];
+            if (current > max) {
+                max = current;
+            }
+        }
+        return (double) max / k;
+    }
+}
